0b830660b2c051d7eefd3403a45ff00b84603f97
zenexity <zenexity@zenexity-formation.(none)>
2010-11-30 17:55:29 +0100

    day 2

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,4 +8,18 @@ public class Application extends Controller {
         render();
     }
 
+    public static void eca() {
+        render();
+    }
+
+    public static void sdr() {
+        render();
+    }
+    
+    public static void journalDeBord() {
+        render();
+    }
+     public static void  rdj() {
+        render();
+    }
 }
